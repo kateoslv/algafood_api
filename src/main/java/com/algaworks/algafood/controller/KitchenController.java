2bd979d56a8cfed8b20da1a6d38fb07bbf790b30
kateoslv <kattyolv@gmail.com>
2023-07-20 00:31:34 +0100

    Implementation of put method.

--- src/main/java/com/algaworks/algafood/controller/KitchenController.java
+++ src/main/java/com/algaworks/algafood/controller/KitchenController.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -54,5 +56,21 @@ public class KitchenController {
 	public Kitchen save(@RequestBody Kitchen kitchen) {
 		
 		return kitchenRepository.save(kitchen);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Kitchen> update(@PathVariable("id") Long id,
+			@RequestBody Kitchen kitchen) {
+		
+		Kitchen kitchenFound = kitchenRepository.findById(id);
+		
+		if (kitchenFound != null) {
+			BeanUtils.copyProperties(kitchen, kitchenFound, "id");
+			
+			kitchenFound = kitchenRepository.save(kitchen);
+			return ResponseEntity.ok(kitchenFound);
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 }
