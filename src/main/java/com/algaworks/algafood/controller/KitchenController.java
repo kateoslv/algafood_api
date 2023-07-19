71b257fedb95b9efbf3fd81b6d6b33fb8d739f9e
kateoslv <kattyolv@gmail.com>
2023-07-20 00:21:05 +0100

    Implementation of post method.

--- src/main/java/com/algaworks/algafood/controller/KitchenController.java
+++ src/main/java/com/algaworks/algafood/controller/KitchenController.java
@@ -3,11 +3,15 @@ package com.algaworks.algafood.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.domain.model.Kitchen;
@@ -43,5 +47,12 @@ public class KitchenController {
 		}
 		
 		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Kitchen save(@RequestBody Kitchen kitchen) {
+		
+		return kitchenRepository.save(kitchen);
 	}
 }
