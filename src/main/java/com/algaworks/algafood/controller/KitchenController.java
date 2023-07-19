6f1aac9b298dbb273d2e8a02e358f1add9fe8f89
kateoslv <kattyolv@gmail.com>
2023-07-20 00:41:23 +0100

    Implementation of delete method.

--- src/main/java/com/algaworks/algafood/controller/KitchenController.java
+++ src/main/java/com/algaworks/algafood/controller/KitchenController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -68,9 +70,28 @@ public class KitchenController {
 			BeanUtils.copyProperties(kitchen, kitchenFound, "id");
 			
 			kitchenFound = kitchenRepository.save(kitchen);
+			
 			return ResponseEntity.ok(kitchenFound);
 		}
 		
 		return ResponseEntity.notFound().build();
 	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Kitchen> remove(@PathVariable("id") Long id) {
+		try {
+			Kitchen kitchenFound = kitchenRepository.findById(id);
+			
+			if (kitchenFound != null) {
+				kitchenRepository.remove(kitchenFound);
+				
+				return ResponseEntity.noContent().build();
+			}
+			return ResponseEntity.notFound().build();
+			
+		} catch (DataIntegrityViolationException e) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).build();
+		}
+	}
+	
 }
