61d44ac005e149474b2868eeaab47ddbc19cde73
kateoslv <kattyolv@gmail.com>
2023-07-20 20:43:15 +0100

    Refactor EntityNotFoundException import.

--- src/main/java/com/algaworks/algafood/domain/service/impl/KitchenServiceImpl.java
+++ src/main/java/com/algaworks/algafood/domain/service/impl/KitchenServiceImpl.java
@@ -1,13 +1,12 @@
 package com.algaworks.algafood.domain.service.impl;
 
-import javax.persistence.EntityNotFoundException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.algaworks.algafood.domain.exception.EntityBeingUsedException;
+import com.algaworks.algafood.domain.exception.EntityNotFoundException;
 import com.algaworks.algafood.domain.model.Kitchen;
 import com.algaworks.algafood.domain.repository.KitchenRepository;
 import com.algaworks.algafood.domain.service.KitchenService;
