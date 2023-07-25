1989dbc39e5d45c36a6c3bbac92fb23f334a1319
kateoslv <kattyolv@gmail.com>
2023-07-25 20:16:57 +0100

    Implementation of update method on RestaurantController.

--- src/main/java/com/algaworks/algafood/controller/RestaurantController.java
+++ src/main/java/com/algaworks/algafood/controller/RestaurantController.java
@@ -2,12 +2,14 @@ package com.algaworks.algafood.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,6 +54,27 @@ public class RestaurantController {
 			
 			return ResponseEntity.status(HttpStatus.CREATED)
 					.body(restaurant);
+		} catch (EntityNotFoundException e) {
+			return ResponseEntity.badRequest()
+					.body(e.getMessage());
+		}
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<?> update(@PathVariable("id") Long id,
+			@RequestBody Restaurant restaurant) {
+		try {
+			Restaurant restaurantFound = restaurantRepository.findById(id);
+			
+			if (restaurantFound != null) {
+				BeanUtils.copyProperties(restaurant, restaurantFound, "id");
+				
+				restaurantFound = restaurantService.save(restaurantFound);
+				
+				return ResponseEntity.ok(restaurantFound);
+			}
+			return ResponseEntity.notFound().build();			
+			
 		} catch (EntityNotFoundException e) {
 			return ResponseEntity.badRequest()
 					.body(e.getMessage());
