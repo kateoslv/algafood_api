601da46d8c76ebd084e573a66b0a9a534725cdde
kateoslv <kattyolv@gmail.com>
2023-07-26 12:17:55 +0100

    Update PaymentMethod to use JpaRepository.

--- src/main/java/com/algaworks/algafood/domain/repository/PaymentMethodRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/PaymentMethodRepository.java
@@ -1,17 +1,11 @@
 package com.algaworks.algafood.domain.repository;
 
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.algaworks.algafood.domain.model.PaymentMethod;
 
-public interface PaymentMethodRepository {
-
-	List<PaymentMethod> list();
-	
-	PaymentMethod findById(Long id);
-	
-	PaymentMethod save(PaymentMethod paymentMethod);
-	
-	void remove(PaymentMethod paymentMethod);
+@Repository
+public interface PaymentMethodRepository extends JpaRepository<PaymentMethod, Long> {
 	
 }
