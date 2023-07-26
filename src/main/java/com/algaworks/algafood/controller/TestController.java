819dafb14638bb720460c348b86e134a3885accf
kateoslv <kattyolv@gmail.com>
2023-07-26 15:58:08 +0100

    Implementation of query methods and TestController class with some examples.

---
+++ src/main/java/com/algaworks/algafood/controller/TestController.java
@@ -0,0 +1,58 @@
+package com.algaworks.algafood.controller;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algafood.domain.model.Restaurant;
+import com.algaworks.algafood.domain.repository.RestaurantRepository;
+
+@RestController
+@RequestMapping("/tests")
+public class TestController {
+
+	@Autowired
+	private RestaurantRepository restaurantRepository;
+
+	@GetMapping("/search_name")
+	public List<Restaurant> findByName(String name) {
+		
+		return restaurantRepository.findByName(name);
+	}
+	
+	@GetMapping("/search_first_name")
+	public Optional<Restaurant> findFirstByName(String name) {
+		
+		return restaurantRepository.findFirstByNameContaining(name);
+	}
+	
+	@GetMapping("/search_name_top2")
+	public List<Restaurant> findTop2ByNameContaining(String name) {
+		
+		return restaurantRepository.findTop2ByNameContaining(name);
+	}
+	
+	@GetMapping("/search_delivery_fee")
+	public List<Restaurant> findByDeliveryFee(BigDecimal initialDeliveryFee, BigDecimal finalDeliveryFee) {
+		
+		return restaurantRepository.findByDeliveryFeeBetween(initialDeliveryFee, finalDeliveryFee);
+	}
+	
+	@GetMapping("/exists_by_name")
+	public boolean findByExistentName(String name) {
+		
+		return restaurantRepository.existsByName(name);
+	}
+	
+	@GetMapping("/count")
+	public int countKitchenId(Long kitchenId) {
+		
+		return restaurantRepository.countByKitchenId(kitchenId);
+	}
+	
+}
--- src/main/java/com/algaworks/algafood/domain/repository/RestaurantRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestaurantRepository.java
@@ -1,5 +1,9 @@
 package com.algaworks.algafood.domain.repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +12,16 @@ import com.algaworks.algafood.domain.model.Restaurant;
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
+	List<Restaurant> findByName(String name);
+	
+	Optional<Restaurant> findFirstByNameContaining(String name);
+
+	List<Restaurant> findTop2ByNameContaining(String name);
+	
+	List<Restaurant> findByDeliveryFeeBetween(BigDecimal initialDeliveryFee, BigDecimal finalDeliveryFee);
+	
+	boolean existsByName(String name);
+	
+	int countByKitchenId(Long kitchenId);
+	
 }
