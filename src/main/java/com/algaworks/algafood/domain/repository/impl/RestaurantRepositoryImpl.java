63cfb8c9d33615721380b9dda1b0025e0dea6483
kateoslv <kattyolv@gmail.com>
2023-07-26 10:12:24 +0100

    Implementation of transactional annotation on remove method.

--- src/main/java/com/algaworks/algafood/domain/repository/impl/RestaurantRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/domain/repository/impl/RestaurantRepositoryImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -34,9 +35,14 @@ public class RestaurantRepositoryImpl implements RestaurantRepository {
 		return manager.merge(restaurant);
 	}
 
+	@Transactional
 	@Override
 	public void remove(Long id) {
 		Restaurant restaurant = findById(id);
+		
+		if (restaurant == null) {
+			throw new EmptyResultDataAccessException(1);
+		}
 		manager.remove(restaurant); 
 	}
 
